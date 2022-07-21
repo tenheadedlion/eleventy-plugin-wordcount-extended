8aaf74859a8d7546c12d6d3ede226781d17ac195
tenheadedlion <16246256+tenheadedlion@users.noreply.github.com>
2022-07-21 09:16:53 +0800

    Rewrite after reading some material

--- src/wordcount.js
+++ src/wordcount.js
@@ -3,34 +3,52 @@ function wordcount(str) {
   return str.map(wordcountWithoutSpace).reduce((prev, curr) => prev + curr, 0);
 }
 
+// Perform a not exactly acurate counting of words
+// ---
+// The original Nunjuck `wordcount` filter separates words by splitting them by space, 
+// but this doesn't work in case of CJK (Chinese, Janpanse, Korean) 
+//  and the some other languages that follow a similar convention,
+// they need a more complex mechanism to detect word boundaries, including dictionary lookup.
+// But in practice, the word count of a blog is often less important, 
+//  and the definition of word can be ambiguous due to the existence of non-natural languages,
+// so keep the code simple and fast, this function will parse each high code point character as a word,
+// as a trade off, the result is possibly inacurate
 function wordcountWithoutSpace(str) {
-  // inspired by
-  //    - https://stackoverflow.com/a/12206089/19406298
-  //    - https://stackoverflow.com/a/20399479/19406298
-  //    - https://stackoverflow.com/a/62898106/19406298
   var cnt = 0;
   var i = 0;
-  var end = str.length;
+  var end = str.length; // JS uses UTF-16 string format, meaning each unit has 2 bytes
   while (i < end) {
     var code = str.charCodeAt(i);
-    if (code <= 0x7ff) {
+    if (code < 0x2e80) { // Chose the number from U+2E80..U+2EFF	CJK Radicals Supplement
       do {
         i += 1;
         code = str.charCodeAt(i);
-      } while (i < end && code <= 0x7ff);
+      } while (i < end && code <= 0x2e80);
+      cnt += 1;
+    } else if (code >= 0x2e80 && code <= 0x9fff) {
+      // CJK, the range is not acurate
+      cnt += 1;
+      i += 1;
     } else if (code >= 0xd800 && code <= 0xdfff) {
-      // Surrogate pair: These take 4 bytes in UTF-8 and 2 chars in UCS-2
-      // (Assume next char is the other [valid] half and just skip it)
+      // Surrogate pair: may form an emoji
+      // > "😀".charCodeAt(0).toString(16) // High Surrogates
+      // 'd83d'
+      // > "😀".charCodeAt(1).toString(16) // Low Surrogates
+      // 'de00'
+      cnt += 1;
       i += 2;
-    }
-    else {
+    } else if (code > 0xffff) {
+      // code point in this range take up three to four bytes in UTF-8, 2 bytes in UTF-16
+      cnt += 1;
+      i += 2;
+    } else {
+      // with "\uffff".length === 1, code point less then 0xffff is considered as a word
+      cnt += 1;
       i += 1;
     }
-    cnt += 1;
   }
 
   return cnt;
 }
-
 
 module.exports = wordcount;
